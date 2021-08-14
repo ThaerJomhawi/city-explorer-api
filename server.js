4bd64c64510dfc19eba43655fe9cc382fc0b4841
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-15 01:21:15 +0300

    update

--- server.js
+++ server.js
@@ -6,14 +6,12 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 server.use(cors());
 
-const weatherHandler =require('./controller/weather.controller')
-const movieHandler =require('./controller/movie.controller')
-
+const weatherHandler = require("./controller/weather.controller");
+const movieHandler = require("./controller/movie.controller");
 
 server.get("/weather/:lat/:lon", weatherHandler);
 
-server.get("/movie/:city_name", movieHandler )
-
+server.get("/movie/:city_name", movieHandler);
 
 server.listen(PORT, () => {
   console.log(`I am a live at  ${PORT}`);
@@ -22,9 +20,3 @@ server.listen(PORT, () => {
 server.get("/", (req, res) => {
   res.send("Hello World");
 });
-
-
-
-
-
-
